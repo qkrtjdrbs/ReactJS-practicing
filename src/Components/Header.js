1b62454d29665761eea8b10aff596dcec2cda2e0
username <user@email.mail>
2021-02-06 15:17:59 +0900

    second commit

---
+++ src/Components/Header.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header`
+  color: whitesmoke;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+  background-color: rgba(20, 20, 20, 0.7);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 5px solid
+    ${(props) => (props.current ? "#DB4455" : "transparent")};
+`;
+
+const SLink = styled(Link)`
+  &:hover {
+    color: orange;
+  }
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
---
+++ src/Routes/Search.js
@@ -0,0 +1,1 @@
+export default () => <h1>SEARCH</h1>;
