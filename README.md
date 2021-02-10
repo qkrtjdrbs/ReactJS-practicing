3e3efbd2dcabc2af930b065254013b3ad58ef840
qkrtjdrbs <notlce@naver.com>
2021-02-10 16:15:43 +0900

    get poster api and implement detail

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Learning React
 - TV shows
 - Search
 - Details
+
+## API Verbs
+
+- [x] Now Playing (movie)
+- [x] Top Rated (movie)
+- [x] Popular (tv, movie)
+- [x] Upcoming (tv, movie)
+- [x] Airing (tv)
+- [x] TV show detail
+- [x] Movie detail
+- [x] Search Movies, TV
---
+++ src/Routes/Home/HomePresenter.js
@@ -0,0 +1,75 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing Movies">
+          {nowPlaying.map((movie) => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map((movie) => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map((movie) => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.original_title}
+              imageUrl={movie.poster_path}
+              rating={movie.vote_average}
+              isMovie={true}
+              year={movie.release_date && movie.release_date.substring(0, 4)}
+            />
+          ))}
+        </Section>
+      )}
+      {error && <Message color="#e74c3c" text={error}></Message>}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
+  upcoming: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
